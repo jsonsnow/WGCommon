4b4162ca0910837b566c57c2c5dc0a97d9e44730
jsonsnow <1183010734@qq.com>
2021-01-22 18:04:35 +0800

    feat: update extention

--- WGCommon/Classes/WGCommon.h
+++ WGCommon/Classes/WGCommon.h
@@ -5,7 +5,16 @@
 //  Created by chen liang on 2021/1/12.
 //
 
+#ifndef WGCommonBridge_h
+#define WGCommonBridge_h
+
 #import "NSString+WGSize.h"
 #import "UIView+ShapeView.h"
 #import "JSONUtils.h"
 #import "WGSafeDefine.h"
+////弱引用
+//#define weakSelf(type)  __weak typeof(type) weak##type = type;
+////强引用
+//#define strongSelf(type)  __strong typeof(type) type = weak##type;
+
+#endif /* WGCommonBridge_h */
